9100bce55db2cd0b65764466f732f0bf3f512cf1
linda-zyx <lindazhangustc@gmail.com>
2020-08-15 10:59:51 -0400

    first commit2

---
+++ GenericFinal/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericFinal
+{
+    public class Program<T> where T: struct
+    {
+        private List<T> MyDatas { get; set; }
+        public List<T> AddData(T t) {
+            MyDatas.Add(t);
+            return MyDatas;
+        
+        }
+        public List<T> GetData() {
+            return MyDatas.GetRange(3, 1);
+        
+        }
+
+        public List<T> GetSorted()
+        {
+            MyDatas.Sort();
+            MyDatas.Reverse();
+            return MyDatas;
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+      
+    }
+}
